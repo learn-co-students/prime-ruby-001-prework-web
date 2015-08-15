ac6286d6887d148b285503ff3ac23705babf4c55
patrickhsanders <patrickhsanders@gmail.com>
2015-08-15 12:01:00 -0400

    not bad at all

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(num1)
+	
+	i = 2
+	prime = true
+	while i < num1
+		prime = false if num1 % i == 0
+		i += 1
+	end
+	return prime
+end
+
+=begin
+puts prime?(3)
+puts prime?(1)
+puts prime?(2)
+puts prime?(4)
+puts prime?(8)
+puts prime?(13)
+puts prime?(101)
+=end
