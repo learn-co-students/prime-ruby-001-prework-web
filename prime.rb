45d97cee8df91580a6f48e2868e1cfde4c19e6e6
jeffslutzky <jeffslutzky@gmail.com>
2015-08-20 18:15:11 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+require 'pry'
+
+
+
+def prime?(number)
+
+  #  create array from [2 --> Math.sqrt(number).ceil]
+  factors = []
+  for factor in 2..Math.sqrt(number).ceil
+    factors.push(factor)
+  end
+
+
+  factors.each do |factor|
+    if number % factor == 0   # no remainder -> number's divisible by something -> not prime
+      return false            # now we can end the program
+    end # end of 'if' statement
+
+  end # end of 'each' loop
+
+return true
+
+
+end
