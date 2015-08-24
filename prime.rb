a786047c416729a7f11e0812bd77937abe24c211
dougtebay <dougtebay@gmail.com>
2015-08-23 21:37:38 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(integer)
+  number_array = []
+  i = false
+  for number in 2..(integer -1) do
+    number_array << number
+  end
+  number_array.each do |x|
+    if integer % x == 0
+      i = false
+      break
+    else
+      i = true
+    end
+  end
+return i
+end
