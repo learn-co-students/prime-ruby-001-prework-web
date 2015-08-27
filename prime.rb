e8b532e462747a431f1f9195ba14e5bcc84b4e35
Kevin Asehan <kasehan@gmail.com>
2015-08-27 23:56:30 +0000

    Thanks planet math

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num1)
+  new_array = []
+  for num2 in 2..Math.sqrt(num1).ceil
+    new_array << num2
+  end
+  new_array.each do |num2|
+    if num1 % num2 == 0
+      return false
+    end
+  end
+  true
+end
