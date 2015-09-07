056d5a008e3ecfc08a318436929aa365b6681387
BikeshDahal <bdahal2358@gmail.com>
2015-09-07 12:00:34 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(integer)
+    if integer==2
+        return true
+    elsif integer<2
+        return false
+    else
+        number=Array.new
+        number=(2..integer/2).to_a;
+        number. each do |numbers|
+            if integer%numbers==0
+                return false
+                break
+            end
+            # Add  code here!
+        end
+    return true
+    end
+end
