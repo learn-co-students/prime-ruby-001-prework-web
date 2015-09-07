41b5004b083df89cb1239108f5f3f6f19b7ae811
BikeshDahal <bdahal2358@gmail.com>
2015-09-07 12:13:45 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,18 +1,11 @@
 def prime?(integer)
-    if integer==2
-        return true
-    elsif integer<2
-        return false
-    else
-        number=Array.new
         number=(2..integer/2).to_a;
         number. each do |numbers|
             if integer%numbers==0
                 return false
                 break
             end
-            # Add  code here!
         end
+            # Add  code here!
     return true
-    end
 end
