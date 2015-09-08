5c9d4c00fb9703fb4ae009698cb237680e3c161e
eenymini <hhl262@nyu.edu>
2015-09-07 22:30:24 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+array = []
+range = (1 .. number).to_a
+range.each {|x| array << number % x} 
+newarray = array.sort
+if newarray[2] != 0
+  return true
+else
+  return false
+end
+end
