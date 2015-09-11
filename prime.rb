5c0646f7dbb83f0c09b83d0efa728c0f8fefc1a3
preetdha <manpreet.dha@gmail.com>
2015-09-11 16:59:03 -0700

    defined prime number method

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(n)
+    if n <= 1
+        return false
+    elsif n <= 3
+        return true
+    elsif n % 2 == 0 || n % 3 == 0
+        return false
+    else 
+        return true
+    end
+end
