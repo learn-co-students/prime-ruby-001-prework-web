3d2f5db687fd1c906d8721fe59adf18f5cc41e92
chenghw <ho.won.cheng@gmail.com>
2015-09-21 11:27:17 -0400

    primes complete

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  prime_checker = 2
+  top_prime_range = number
+
+  while prime_checker < top_prime_range
+    if number % prime_checker == 0
+      return false
+    else
+      top_prime_range = number/prime_checker
+      prime_checker += 1
+    end
+  end
+  
+  true
+end
