dc77b6d58e84c4c5060bc25685008810d57c706b
ruchiramani <ruchiramani@gmail.com>
2015-09-23 02:15:25 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
+def prime?(num)
 # Add  code here!
+ new_array = []
+ (2...num).each do |x|
+  new_array << x
+ end
+ idx = 0
+ while idx < new_array.length 
+    if (num % new_array[idx]) == 0
+      return false
+    end
+    idx +=1
+  end
+  return true
+
+end
+
+
+
