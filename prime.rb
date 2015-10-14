113d330a5204af7eded35b697d587f9c5006ad1f
Anna Rasshivkina <anna.nigma@yahoo.com>
2015-10-14 16:08:18 -0400

    Add fastest prime algorithm

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+=begin
 def prime?(num)
   for i in 2..Math.sqrt(num)
     if num % i == 0
@@ -5,4 +6,9 @@ def prime?(num)
     end
   end
   true
+end
+=end
+
+def prime?(num)
+  num == 2 || num == 3 || num % 6 == 1 || num % 6 == 5 ? true : false
 end
