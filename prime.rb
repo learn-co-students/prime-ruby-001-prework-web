db7ccd1efaafa102b6afc836c1ac1e781e76d80f
flats <dave@robador.com>
2015-10-17 21:14:13 -0400

    implement simple, inefficient primality test

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(n)
+  if n == 2 then return true
+  elsif n == 3 then return true
+  elsif n % 2 == 0 then return false
+  elsif n % 3 == 0 then return false
+  end
+
+  div = 5
+  while div < n
+    if n % div == 0
+      return false
+    else
+      div += 2
+    end
+  end
+  true
+end
