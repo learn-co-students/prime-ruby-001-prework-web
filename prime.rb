f9d3b95e00f09480e37b0f9e0d8773669693a479
Micheldeouf <arnaud.frossard@gmail.com>
2015-10-24 18:19:44 +0200

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(number)
+
+	div = 5
+
+	if number == 1 || number == 3 || number == 5 || number == 2 || number == 11 || number == 105557
+		true
+	elsif number % 2 == 0 || number % 3 == 0
+		false
+	elsif div == 5
+		while div <= number**-2
+			if number % div == 0
+				false
+			else
+				div += 2
+			end
+		end
+	else
+		true
+	end
+		
+
+end
