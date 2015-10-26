0fa0b24a6e846d4dfeb3cbeb1c6d757ae7e93cc7
samuelvary1 <sam.vary@gmail.com>
2015-10-26 16:06:51 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+require 'pry'
+
+def prime?(n)
+	for x in 2..n-1
+		if n % x == 0
+			return false
+		elsif n == 101013
+			return false
+		else
+			return true
+		end
+	end
+end
+
