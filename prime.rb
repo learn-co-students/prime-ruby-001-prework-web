58a75a6a1b705f86b80db0d41b37f3a9c8f60960
daniellacor <daniella.corricelli@gmail.com>
2015-10-27 11:11:59 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(x)
+ if x % 2 == 0
+  false
+ elsif x == 3
+  true
+ else
+  array = (3..(x-1)).to_a
+  array.each do |i|
+   if x % i == 0
+    return false
+   else
+    return true
+   end
+  end
+ end
+end
