91833e57cb0a7c58570d36bbbc852d387f878a10
kaddae <kai@tilde.io>
2015-10-28 11:23:11 -0700

    MAGIC

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  return false if number<=1
+  def sqrt_whole(x)
+    z=x** 0.5
+    if z%1==0
+      return z
+    else
+      return z.floor
+    end
+  end
+  range=(2..sqrt_whole(number))
+  range.each do |x|
+    return false if number% x== 0
+  end
+  true
+end
