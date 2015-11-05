9820a49779f12787d82ff75f4b9cba3290f007b2
Aemrox <adam.may2164@gmail.com>
2015-11-04 22:59:17 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+def prime? (int)
+  if int <= 1
+    return false
+  elsif int <= 3
+    return true
+  elsif (int % 2 == 0 || int % 3 == 0)
+    return false
+  end
+  i = 5
+  primes = [2,3,i]
+  while i < int
+    if int % i == 0
+      return false
+    end
+    i += 2
+  end
+  return true
+end
