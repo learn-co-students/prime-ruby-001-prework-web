a21992e4b893e868221dddac5a1807752893a554
tuckerbohman5 <tucker.bohman@gmail.com>
2015-11-16 06:34:53 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  if num < 2 
+    return false
+  end
+    array_range = (2..num -1)
+
+  array_range.each do |x|
+    if num % x == 0
+      return false
+    
+    end
+
+
+
+  end
+ true 
+end
