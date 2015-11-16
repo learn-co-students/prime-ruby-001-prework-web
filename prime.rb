7fd9068e952eba10f6da14b1d75b89c5762228a6
iSadia <sadia.bruce@gmail.com>
2015-11-16 17:22:39 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(num)
+  # 0 is not prime (the spec makes this clear)
+  # Primes are divisible by exactly two numbers: 1 and themselves.
+  # Thus, 1 is not prime
+  return false if num < 2
+
+  # 2 is the only even prime number
+  return true if num == 2 
+
+  # Otherwise, we test primality by checking if any number
+  # from 2 to num - 1 divides num evenly
+  (2..num - 1).each do |n|
+    return false if num % n == 0
+  end
+
+  # If none of them do, the number is prime
+  return true
+end
