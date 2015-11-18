fd9c9561f45535da5459b725c17b7c99577f3ac8
github username <github email address>
2015-11-18 16:38:03 -0500

    create prime? method

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime?(number)
+  return false if number == 0 || number == 1 
+  (2..(number - 1)).to_a.each do |num|
+    return false if number % num == 0 
+  end 
+  true 
+end 
