4fcf5e3a96c097e6ac36fadeecfc403d09ecd1e8
dorothym <dorothy.v.moore@gmail.com>
2015-11-20 01:46:11 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(int)
+	if int == 0 || int == 1
+		return false
+	else
+		for i in (2...int) 
+			#puts int % i
+			if int % i == 0
+				return false
+			end
+		end
+	return true
+	end
+end
+
+#prime?(7)
