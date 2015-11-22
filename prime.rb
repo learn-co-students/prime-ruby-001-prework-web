5056f801b0350e45620dd8c84035ea94d773770f
seanliu93 <sean93liu@gmail.com>
2015-11-22 12:51:58 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num <= 3
+    return true
+  elsif num % 2 == 0 or num % 3 == 0
+    return false
+  end
+
+  i = 5
+  while i*i <= num
+    if num % i == 0 or num % (i+2) == 0
+      return false
+    end
+    i = i + 6
+  end
+
+  return true
+end# Add  code here!
