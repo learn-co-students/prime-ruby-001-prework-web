97c3beb2e60bdb4fadd4bad7d6edfa61f9ceef56
danfaeh <danfaeh@gmail.com>
2015-11-22 15:37:26 -0800

    no problemo

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(int)
+  if int % 2 > 0 && int % 3 > 0 && int != 1 || int == 2 || int == 3 
+    puts true
+    true
+  else 
+    puts false
+    false
+  end  
+end
+
