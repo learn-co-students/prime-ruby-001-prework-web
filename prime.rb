93907569c0a97fe5bc77007f19a18db582bf1bbb
Abowlofapples <dominiquealiciabrown@gmail.com>
2015-11-23 00:13:33 -0500

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(integer)
+if integer % 2 > 0 && integer % 3 > 0 && integer != 1 || integer == 2 || integer == 3
+  puts true
+  true
+else
+  puts false
+  false
+end
+
+end
