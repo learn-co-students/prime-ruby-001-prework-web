eeda689152e0d1c2fe79999f722bcba6656afa7a
Amger <albertgertskis@gmail.com>
2015-11-23 23:01:27 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(your_integer)
+  #convert the range of all numbers to your number to an array
+  numbers = (1..your_integer).to_a
+
+  #counter to catch divisors for user's number
+  divisor_counter = 0
+
+  numbers.each do |element|
+    #if user number remainder is 0, add to counter; otherwise don't
+    your_integer % element == 0 ? divisor_counter += 1 : divisor_counter
+  end
+
+  #if user number has exactly 2 divisors, it is prime; otherwise it isn't prime
+  divisor_counter == 2 ? true : false
+end 
