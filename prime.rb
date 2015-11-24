f01817ddabf2808198424130655dc9512cdacb45
burkefielder <burke.fielder@gmail.com>
2015-11-23 19:29:46 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  elsif number % 2 == 0 || number % 3 == 0
+    return false
+  else
+    return true
+  end
+end
+
+ 
