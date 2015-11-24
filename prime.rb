76a0f21942467805a02679db152d1ded678bac94
jamesevers <jevers@gm.slc.edu>
2015-11-24 18:25:44 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+ def prime?(n)
+ 	prime = true
+ 	if n < 2
+ 		prime = false
+ 	elsif
+	 	(2...n).each { |i|
+	 		prime = false if Float(n) % i == 0
+	 	}
+	 else
+	 	prime = true
+	 end
+	 prime
+ end
