0dcf789dfdef76d3d3eb364ed74ab9f67ad86e4a
zdg2102 <zacharydgarcia@gmail.com>
2015-11-26 04:32:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  if num == 0 || num == 1
+    return false
+  end
+  test_factor = num - 1
+  while test_factor > 1
+    quotient = num.to_f / test_factor
+    if quotient == quotient.floor
+      return false
+    end
+    test_factor -= 1
+  end
+  true
+end
