f4bd682044e1287a5096c2cef1dbe3bf52245f5b
tintinmar <wangmartinxy@gmail.com>
2015-12-01 21:08:46 -0500

    solved

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(integer)
+  i = false
+  array = []
+  range = 2..(integer-1)
+  for i in range
+    array << i
+  end
+  array.each do |x|
+    if integer % x == 0
+      i = false
+      break
+    else i = true
+    end
+  end
+  return i 
+end
