4f9d99c090dacf2bf2b50a676cd90f4d804ae365
sofiajeanne <sofia.rogg@gmail.com>
2015-12-03 14:03:07 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  divisors = *(2..num - 1)
+  remainders = []
+  divisors.each do |divisor|
+    remainder = num % divisor
+    remainders << remainder
+  end
+  if num > 1 then remainders.all? {|remainder| remainder != 0}
+  else
+    false
+  end
+end
