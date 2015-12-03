e7f602189d2926219e6e2fcd5504c755782becf7
wmhall001 <wmhall001@gmail.com>
2015-12-02 20:02:32 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+def prime?(number)
+  divisors = 0 
+
+  for i in 1..number do 
+    if number % i == 0
+      divisors += 1
+    end
+  end
+
+  if divisors == 2
+    return true
+  else 
+    return false
+  end
+
+end
