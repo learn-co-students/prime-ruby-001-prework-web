d5875220722f88f5fab217b9580d5f0870fbaff9
OrthoStice <shelleyhicklin@hotmail.com>
2015-12-03 15:31:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  (2..Math.sqrt(number)).each { |i|
+    if number % i == 0 && i < number
+      return false
+  end
+  }
+  if number < 2
+    return false
+  end
+  true
+  end
