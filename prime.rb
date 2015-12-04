8d940c783068a53a990fdf306d1e454441dfcb2b
Saul Fernandez <fsaul16@yahoo.com>
2015-12-04 16:21:23 +0000

    prime num

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(numbers)
+  divisors = *(2..numbers - 1)
+  remainders = []
+  divisors.each do |divisor|
+    remainder = numbers % divisor
+    remainders << remainder
+  end
+  if numbers > 1 then remainders.all? {|remainder| remainder != 0}
+  else
+    false
+  end
+end
