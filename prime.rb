9cf2d3731eac947682b99594fba0998231a9661d
mindyyuan <mindyuan@gmail.com>
2015-12-06 21:11:17 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(int)
+
+    if int<=1
+        return false
+    end
+
+    n=2
+    while n<int
+        if int % n == 0
+            return false
+            break
+        end
+        n+=1
+    end
+return true
+end
