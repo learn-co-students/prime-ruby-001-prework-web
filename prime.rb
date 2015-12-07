38e90926c886c6ea5e88b3daecbe59b34cc7f2c2
devinburnette <devin.burnette@gmail.com>
2015-12-07 04:17:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+  return false if num <= 1
+  max = Math.sqrt(num)
+  (2..max).each do |i|
+    if (num % i) == 0
+      return false
+    end
+  end
+  return true
+end
