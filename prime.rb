d1f04349d835880dd13930af2f2654a0aa141ddf
Adam King <adam.king0126@gmail.com>
2015-12-08 20:52:11 +0000

    solve exercise

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(n)
+  return true if (n == 2 || n == 3)
+  return false if (n % 2 == 0 || n % 3 == 0 || n == 1)
+
+  i = 5
+  w = 2
+  while i * i <= n do
+    return false if n % i == 0
+    i += w
+    w = 6 - w
+  end
+
+  true
+end
