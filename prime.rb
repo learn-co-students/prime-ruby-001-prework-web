3e47f58130c2fd5e28945f248dc51268a3222ddb
gncurran <gnc115@gmail.com>
2015-12-09 13:43:49 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n)
+  range = (2..Math.sqrt(n)).to_a
+  range.each do |x|
+    if n % x == 0 && x < n
+      return false
+    end
+  end
+  if n < 2
+    return false
+  end
+ true
+end
