138cc24d12bd807f90ba0b0e02999558ac7fe13f
Nathaniel Miller <dukeoflaser@gmail.com>
2015-12-12 23:04:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(n)
+  if n < 2
+    false
+  elsif (2..3) === n
+    true
+  elsif n % 2 == 0 || n % 3 == 0
+    false
+  else
+    i = 5
+    w = 2
+    while i * i <=n
+      if n % i == 0
+        false
+      end
+      i += w
+      w = 6 - w
+    end
+    true
+  end
+end
