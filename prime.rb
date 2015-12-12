cb969bd8b7108b013672474381768b20c563a349
Blake Johnston <johnstonbl01@gmail.com>
2015-12-11 19:07:23 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime? (num)
+   is_prime = true
+   if num <= 1
+
+      is_prime = false
+
+   else
+
+      for int in 2..(num - 1) do
+         if num % int == 0 || num <= 1
+            is_prime = false
+         end
+      end
+
+   end
+   
+   is_prime
+
+end
