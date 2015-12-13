44c4be318f71ca964bf4242cfaaf728ab846ce71
tttra <tothutra@gmail.com>
2015-12-13 14:50:59 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(x)
+  if x == 2 || x == 3 || x ==5 || x == 7
+    true
+  elsif x < 2
+    false
+  elsif !(x %2 == 0 || x %3 == 0 || x %5 == 0 || x %7 == 0)
+    true
+  else
+    false
+  end
+end
