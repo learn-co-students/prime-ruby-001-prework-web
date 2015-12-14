ff26f64a79eb25c6882949071bb0a8de42fdda20
TyJuanABrooks <tyjuanabrooks@gmail.com>
2015-12-14 02:12:57 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  elsif n % 2 == 0 || n % 3 == 0
+    return false
+  else
+    return true
+  end
+end
