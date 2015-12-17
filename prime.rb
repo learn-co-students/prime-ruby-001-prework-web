7dc17a7b8b905e7a184e47d996c25f780dfddc3c
cklynch2 <cklynch2@gmail.com>
2015-12-17 06:25:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+  divisors = (2...int).to_a
+  remainders = []
+  divisors.each do |x|
+    remainder = int % x
+    remainders.push(remainder)
+  end
+  if int > 1
+    !remainders.include?(0)
+  else int <= 1
+    false
+  end
+end
