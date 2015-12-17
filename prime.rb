4fa1c807565d34805fd89887f4574158fc231144
Shirley Berry <shirleylberry@gmail.com>
2015-12-17 00:33:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+def prime?(num)
+    if num < 2
+        return false
+    end
+    (2...num).each do |number|
+        if num % number == 0
+            return false
+        end
+    end
+#     puts "prime"
+    return true
+end
+
+prime?(10)
+prime?(7)
