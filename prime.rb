1ca3d832001a52d29a85ba7da906ab6a602ce9c7
Juan Ruiz <juankboards@hotmail.com>
2015-12-18 01:33:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(number)
+  return false if number <=1
+  return true if number==2
+  (2...number).each do |divisor|
+    return false if number%divisor==0
+  end
+  return true
+end
