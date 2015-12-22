7e92dc19dfb59fb4b958cc71771331e35abaf122
francis-m-kim <francis.m.kim@gmail.com>
2015-12-22 14:18:11 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  if number == 0 || number == 1
+    return false
+  end
+  foundDivider = false
+   for i in 2..(number - 1)
+    foundDivider = (number % i == 0) || foundDivider
+    break if foundDivider == true
+   end
+   !foundDivider
+ end
