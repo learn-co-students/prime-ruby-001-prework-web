bb65a7bfab435198182e35fe2c6b0658d71b84b9
andrecreighton <acreighton92@gmail.com>
2015-12-22 02:25:20 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(number)
+
+if number == 1 || number == 0
+    return false
+end
+
+key = 2
+
+    while key < number
+        divisible = ((number % key) == 0)
+        if divisible
+            return false
+        end
+    
+      key += 1
+
+    end
+    true
+end
