1468af7b6df3d5ac16ef2d8ca0caf11a8dca54f2
GHACK101 <jakegkaplan@gmail.com>
2015-12-22 15:49:37 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  prime = true
+  if num == 0 || num == 1
+    prime = false
+  end
+  for x in (2...num)
+    if num % x == 0
+      prime = false
+    end
+  end
+  prime
+end
