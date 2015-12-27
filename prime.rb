c3a40f33f4f8556d6d7767489edf55105f65aec7
MiguelTepale <miguel.tepale@gmail.com>
2015-12-27 01:05:03 +0000

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+# Add code here!
+def prime?(num)
+  if (num < 2)
+    return false
+  elsif (num < 4)
+end
+    array = Array(2..(num-1))
+    array.each do |x|
+      if (num % x == 0)
+        return false
+      end
+   end
+  return true
+end
