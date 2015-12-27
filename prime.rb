3bd9347f252beadef5c21e5cd411a5613ebca407
harichandansingh <singh.harichandan@gmail.com>
2015-12-27 14:07:38 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(num)
+  limit = (Math.sqrt(num).ceil)
+    for i in 2..limit do
+        break if num % i == 0
+        return true if i == Math.sqrt(num).ceil   
+    end
+    return false
+end
