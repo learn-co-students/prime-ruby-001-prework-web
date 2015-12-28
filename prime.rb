90fc7728e2e340ccb8e414ec63f59b2dd11b4510
WilliamAG <william.a.gough@gmail.com>
2015-12-28 14:13:12 -0800

    Done

--- prime.rb
+++ prime.rb
@@ -4,11 +4,17 @@ def prime?(n)
   elsif n < 4
     return true
   end
-  array = Array(2..(n-1))
-  array.each do |x|
+  for x in 2..(n-1)
     if n % x == 0
      return false
     end
   end
   return true
+  # array = Array(2..(n-1))
+  # array.each do |x|
+  #   if n % x == 0
+  #    return false
+  #   end
+  # end
+  # return true
 end
