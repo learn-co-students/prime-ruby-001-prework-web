41cbd303dc3e4e25d42fb666f2d66b31971c6217
franco84 <franco84@gmail.com>
2015-12-29 01:16:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime?(primetest)
+  return false if primetest < 2
+  (2..Math.sqrt(primetest)).each do |number|
+    return false if primetest % number == 0
+  end
+  return true
+end
