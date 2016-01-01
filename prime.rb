6c58ebd6cb28b6a1e694f3721a2636bd6f90f255
gregmarquet <marquet.greg@gmail.com>
2016-01-01 18:13:40 -0500

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(nums)
+    if nums < 2
+        return false
+    end
+    for d in 2..(nums - 1)
+       if nums % d == 0
+        return false
+       end
+    end
+
+    true
+end
