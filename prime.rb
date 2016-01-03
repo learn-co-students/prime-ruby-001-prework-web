b5755210c5b7bedb5f3050063bb5c617a5cb0677
Harrison Lavin <Harrison.lavin@gmail.com>
2016-01-03 20:53:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+    #checks modulo division of all numbers between 1 and number.
+    #returns false if  number % n is true if number != 1 or number
+    n=1
+  if number== 0 || number== 1
+      return false
+    end
+
+    while n <= number do
+        if ((n != 1) && (n != number) && (number % n == 0))
+          return false
+        end
+        n+=1
+    end
+  return true
+end
