3312706f7492c235176e9ca505d911afdecac5bf
akhinchey <akhinchey@gmail.com>
2016-01-03 16:21:45 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+ if num < 2
+  return false
+end
+
+  array = (2...num).to_a
+  array.each do |n|
+    if num % n == 0
+      return false
+    end
+  end
+  return true
+end
