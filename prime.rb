920bd1cb6a3bff997daac4435ca83478e55b5ade
bfitzpa5 <brendanjfitzpatrick0@gmail.com>
2016-01-09 21:15:36 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(x)
+ new_ints = []
+    ints = [*2..Math.sqrt(x)] 
+    ints.each do |i|
+      new_ints << x % i
+    end
+  new_ints
+
+  if x == 2
+    return true
+  elsif x == 3
+    return true
+  elsif x > 3 and new_ints.include?(0) == false
+    return true
+  else
+    return false
+  end
+end
