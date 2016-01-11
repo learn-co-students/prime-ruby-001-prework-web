5cd53ea6447f3a72e859eaf212f761ebd661130a
SaiedAbbasi <saiedabbasijr@gmail.com>
2016-01-11 15:45:12 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(x)
+  if x == 0 || x == 1
+    return false
+  end
+  i = 2
+  while i < x
+    is_divisible = ((x % i) == 0)
+    if is_divisible
+  
+      return false
+    end
+
+    i += 1
+  end
+  true
+end
