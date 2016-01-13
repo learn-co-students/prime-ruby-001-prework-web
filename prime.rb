42e8affd61d6aac69464930b7eb3f2a9b6f225ad
nigeriangirl <onye@smmaven.com>
2016-01-13 06:08:25 -0800

    Prime Ruby

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime? (num)
+  if num <= 1
+    false
+  elsif num <= 3
+    true
+  elsif num % 2 == 0 || num % 3 == 0
+    false
+  else
+    true
+  end
+end
+
+    
+    
