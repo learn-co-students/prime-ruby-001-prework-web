48afc2031f2d2b99664aaae2ceed487fe504b864
HarlemSquirrel <wittyman37@yahoo.com>
2016-01-14 22:25:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  elsif n % 2 == 0 || n % 3 == 0
+    return false
+  end
+
+  i = 5
+  while i * i <= n do
+    if n % i == 0 || n % (i + 2) == 0
+      return false
+    end
+    i += 6
+  end
+  return true
+end
