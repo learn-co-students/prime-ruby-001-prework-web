c148dad0d6b4a9587f2b0b5c4aa646b4b88b0bc7
chrism81 <chrism81@msn.com>
2016-01-15 15:33:34 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
+def instantiate_new_array
+  []
+end
+
+def prime?(number)
+  l= instantiate_new_array
+  x=2
+  while x!= number
+    l << x
+    x+=1
+  end
+
+  
+  if l.each {|x| number % x}!=0
+    true
+  end
+    
+  #end number % x!=0 == true return true else false end}
+end
+
 # Add  code here!
