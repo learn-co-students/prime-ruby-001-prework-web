5c4659e3850411a0b3f9fb7370350166d8107433
chrism81 <chrism81@msn.com>
2016-01-15 17:44:11 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -4,18 +4,32 @@ end
 
 def prime?(number)
   l= instantiate_new_array
+  k= instantiate_new_array
   x=2
-  while x!= number
-    l << x
+
+  while x!= 9
+    if (number!=x)
+      l << x
+    end
     x+=1
   end
 
-  
-  if l.each {|x| number % x}!=0
-    true
+  if number==0||number==1
+    return false
   end
-    
-  #end number % x!=0 == true return true else false end}
+
+  if number==2||number==3
+    return true
+  end
+
+  l.each {|x| k << number % x }
+
+ 
+ if k.include?(0)==true 
+  return false
+else return true
 end
 
-# Add  code here!
+ 
+ 
+end
