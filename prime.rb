c1e04af2990a08443bcd25b80b668869297f574a
laushinka <laurie.malau@gmail.com>
2016-01-17 23:03:09 +0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(x)
+  if x < 2
+    return false
+  elsif (x == 2) || (x == 3)
+    return true
+  elsif (x % 2 == 0) || (x % 3 == 0) || (x % 5 == 0) || (x % 7 == 0) || (x % 11 == 0)
+    return false
+  else
+    return true
+  end
+end
