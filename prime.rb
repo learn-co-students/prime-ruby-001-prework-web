2a7cb085af9f44ca8b4d20df97d159a63d7082da
orobert1 <orobert@risd.edu>
2016-01-18 14:13:57 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
+def prime?(integer)
+  if(integer==0)
+    return false
+  end
+  if(integer==1)
+    return false
+  end
+  for var in 2..integer-1 
+    if integer%var==0
+      return false
+    end
+  end
+  return true
+end
+
+
 # Add  code here!
