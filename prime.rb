440a7c0a07286d58d9cbfd65e8630897044d24bb
eessex <eve.essex@gmail.com>
2016-01-18 04:30:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,14 @@
 # Add  code here!
 
-def prime?(i)
-  if i % i == 0 
-    return true
-  else
+def prime?(number)
+  if number > 1
+    for numbers in 2..(number-1)
+      if number % numbers == 0
+      return false
+      end
+    end
+  elsif number == 0 || number == 1
     return false
   end
+    return true
 end
