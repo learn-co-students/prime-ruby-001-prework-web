58c9e1497790846a003ead4bf2cd6414b79693dc
smernick <smernick3@gmail.com>
2016-01-21 15:41:12 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,5 @@
-# Add  code here!
+def prime?(integer)
+  return false if integer < 2 # returns false for 0,1 (which are exceptions)
+  (2..integer/2).none? {|i| integer % i == 0} # calcualtes the remainder and determines prime
+end
+
