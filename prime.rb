4b3280e45f488df9d690e4860eaf5829a2d37a93
dcarrano <dcarrano@gmail.com>
2016-01-23 03:16:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(number)
+
+  case
+  when number < 2
+    false
+  when number == 2
+    true
+  when number == 3
+    true
+  when number == 4
+    false
+  else
+    i = 2
+    until (number % i == 0) || Math.sqrt(number).floor == i
+      i += 1
+      end
+    if Math.sqrt(number).floor == i then true else false end
+  end
+
+end
