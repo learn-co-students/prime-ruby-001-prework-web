9c1dc861754b7306d23190b44ab196107b5290f2
jjseymour <jjseymour@gmail.com>
2016-01-23 18:03:15 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,3 @@
-# Add  code here!
+def prime?(are_you_primed)
+  are_you_primed % 2 == 0 || (are_you_primed % 3 == 0 && are_you_primed != 3) || are_you_primed == 0 || are_you_primed == 1 ? false : true
+end
