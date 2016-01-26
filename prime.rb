7a2f1d29d8adf11d447749c90b8d03b509247116
amandaprinzo@gmail.com <amandaprinzo@gmail.com>
2016-01-25 19:01:50 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,4 +1,5 @@
 def prime?(num)
+  return false if num <=1
   (2...num).each do |divisor|
       return false if num % divisor == 0
       end
--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -9,8 +9,8 @@ describe "prime?" do
   end
 
   it 'returns false for non-prime numbers' do
-    #expect(prime?(0)).to be(false)
-    #expect(prime?(1)).to be(false)
+    expect(prime?(0)).to be(false)
+    expect(prime?(1)).to be(false)
     expect(prime?(4)).to be(false)
     expect(prime?(40)).to be(false)
     expect(prime?(101013)).to be(false)
