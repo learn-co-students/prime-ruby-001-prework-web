f5c51186ac223fad66160f7e666630c01b0a0333
gs2589 <gabriel.seidner@gmail.com>
2016-01-29 16:27:54 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -23,3 +23,32 @@ end
 return true
 
 end# Add  code here!
+
+
+
+
+def prime2?(num)
+
+# check to see if num is greater than 1
+#check to see if num=2
+# check to see if number is divisble by 2 or 3
+if num<2 then return false
+elsif num==2 || num==3 then return true   #IS THIS REALLY NEEDED? CAN I GENERALIZE IT?
+end 
+
+# check to see if num is divisible by 2 or 3
+if num%2==0 || num%3==0 then return false
+end 
+
+# check to see if num % 6k + - 1 where 6k + - <=sqrt(num) ==0 
+ 
+ k=2
+ while k <= Math.sqrt(num)
+  if num % k ==0  then return false
+  end
+  k+=1
+end
+return true
+
+end# Add  code here!
+
