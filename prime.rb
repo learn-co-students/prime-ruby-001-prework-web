74c1c0419b37c50ac1ec77199a701b114c62c5e2
erinadora <eringoncer@gmail.com>
2016-01-28 21:11:33 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(num)
+         return false if num <= 1
+        2.upto(Math.sqrt(num).to_i) do |x|
+            return false if num%x == 0
+        end 
+        true
+    end 
+
+    def next_prime
+        n = self+1
+        n = n + 1 until n.prime?
+        n   
+    end 
+
+
+
+
+
