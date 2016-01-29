601cbd1dd51d616f589224f05625271d8b480fb9
namimi928 <soyongnam@gmail.com>
2016-01-28 22:27:19 -0500

    no message

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+ def prime? (num)
+  if num <= 2
+    return false
+  else
+    for d in 2..(num - 1)
+      if (num % d) == 0
+      return false
+      end
+    end
+   return true
+  end
+end
+
