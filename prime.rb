0526296e2349bde4cfd965d5c61edb55c1a4ccc9
agdavid <antonio.g.david@gmail.com>
2016-02-04 16:23:44 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(integer)
+  range = (2..integer).to_a
+  new_array = []
+  range.each do |range_number|
+    new_array_value = integer%range_number
+    new_array.push(new_array_value)
+  end
+  if integer <= 1
+    return false
+  else
+      if new_array.count(0) == 1
+        return true  
+      else
+        return false
+      end
+  end
+end
