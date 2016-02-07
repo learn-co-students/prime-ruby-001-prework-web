5a54ded6e987010db056d6a3e4746161830fdc71
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2016-02-07 12:15:02 +0200

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+# Add  code here!()
+def prime?(num)
+	not_prime = false
+	is_prime = true
+  return not_prime if num < 2
+  i = 2
+  while i < num
+    return not_prime if num % i == 0
+    i = i + 1
+  end
+  return is_prime
+end
