61c9b053ecfd8e6b9cb0f192d0349c6123d362fe
jakeekaj <jakemalto2014@gmail.com>
2016-02-11 05:22:17 +0000

    Done.

--- primetest.rb
+++ primetest.rb
@@ -7,3 +7,18 @@ if x == 2 || x == 3 || x == 5 || x == 7
   return true
   end
 end
+
+
+
+x=  0
+y = []
+
+while x < 200 do
+  if prime?(x)
+    puts "#{x} is a Prime number"
+    y << x
+  end
+  x+=1
+end
+
+print y
