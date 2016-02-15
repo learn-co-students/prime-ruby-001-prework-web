6f70573b90bf7ecfc7df9b0ad89e8640ace59447
blochbr <blochbr@gmail.com>
2016-02-15 03:11:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(integer)
+  count_zeros = 0
+  for x in integer.downto(1)
+    check_remainder = integer%x
+      if check_remainder == 0
+        count_zeros = count_zeros + 1
+      end
+  end
+      if count_zeros == 2
+        puts "Prime"
+        return true
+      else
+        puts "Not Prime"
+        return false
+      end
+end
+
+prime?(0)
+
+
+
+
