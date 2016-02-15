c0b7f8ec7a32b731159a9662781044d473f48e16
Geoff Bass <geoff.c.bass@gmail.com>
2016-02-15 15:35:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  return false if num < 2
+  count = 2
+  while count < num
+    if num % count == 0
+      return false
+    end
+    count += 1
+  end
+  return true
+end
