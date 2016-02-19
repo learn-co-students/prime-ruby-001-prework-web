e92536bb015bbf04ec3f1c37585e46ec8048b21d
Mayank Mishra <mayank.law@gmail.com>
2016-02-19 17:02:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+
+  index=2
+while index < num
+  if num%index == 0
+    return false
+  end
+  index+=1
+end
+
+return true
+  
+end
+
