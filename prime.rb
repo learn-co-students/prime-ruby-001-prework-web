48731f156e7cf71657066976eb762f58becdf566
gnwankwo <gnwankwo03@gmail.com>
2016-02-19 21:22:25 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(num)
+  num= Float(num)
+  squrt=Math.sqrt(num)
+  if num <= 2.0
+    return false
+  elsif (squrt%3) == 0.0
+    return false
+  elsif num%2 ==0.0
+    return false
+  elsif num==3.0
+    return true
+  elsif num%3==0.0
+    return false
+  else
+    return true
+  end
+end
+
