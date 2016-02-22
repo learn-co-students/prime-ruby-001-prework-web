f0144d566c3d9e00d910e3b0fde1eba79ae7f09f
kwebster2 <kevin.r.webster@gmail.com>
2016-02-21 22:51:55 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+range = (1..int).to_a
+remainders = []
+
+  range.each do |divisor|
+     if int % divisor != 0
+      remainders << int % divisor
+    end
+  end
+remainders.size == int - 2
+end
+
