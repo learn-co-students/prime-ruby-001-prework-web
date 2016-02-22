a5039872d668a42b4f75610f0ce19244cc1b31b2
PoloPro <wesleybdavis@gmail.com>
2016-02-22 03:30:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(value)
+
+  case value
+    when 0
+      return false
+    when 1
+      return false
+    when 2
+      return true
+  end
+
+  squareroot = Math.sqrt(value).to_i
+
+  (2..squareroot).each do |number|
+    if value % number == 0
+      return false
+    end
+  end
+
+  true
+
+end
