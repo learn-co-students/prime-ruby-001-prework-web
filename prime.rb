eca0ec9563190ac8d41399c6c2115f5a6f024b6e
chasejthompson <chase@theprospectgroup.com>
2016-02-23 15:13:43 +0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
 # Add  code here!
+# https://github.com/ruby/ruby/blob/trunk/lib/prime.rb
+def prime?(int)
+  return false if int <= 1
+  Math.sqrt(int).to_i.downto(2).each {|i| return false if int % i == 0}
+  true
+end
