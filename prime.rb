5299cab1489fa2c5214b4cab436c2588ab1394bc
robinackers <robinackers@gmail.com>
2016-02-26 17:02:54 -0500

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+# Add code here!
+def prime?(num)
+  2.upto(Math.sqrt(num).ceil) do |i|
+        break if num%i==0
+        return true if i==Math.sqrt(num).ceil   
+    end
+    return false
+end
+
+
