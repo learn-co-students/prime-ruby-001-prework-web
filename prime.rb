4cc843b7fc25c755374987fd24aea1563328b44e
czarnega <c.zarnegar@gmail.com>
2016-02-25 17:18:10 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  if num == 0 || num == 1
+    false
+  elsif num > 0 && num < 4
+    true
+  else
+    numArray = (2..(num-1)).to_a
+    
+    numArray.each {|x|
+      if num % x == 0
+        return false
+      end
+    }
+
+    true
+  end
+end
