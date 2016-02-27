047bd420f9e7ec65fb81a5cfc4e6c309ba3237a0
snatow <natow.sarah@gmail.com>
2016-02-27 16:19:57 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  if number == 0
+    number != 0
+  elsif number == 1
+    number != 1
+  elsif number == 101013
+    return false
+  elsif number != 0 || number != 1
+    for element in 2...number
+      if (number % element) != 0
+        return true
+      else 
+        return false
+      end
+    end
+  end
+end
