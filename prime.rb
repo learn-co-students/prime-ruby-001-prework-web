0626d4dc0f6215e3828084dea900f867add4460c
LibraHampton <LibraHampton@live.com>
2016-02-27 21:56:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(n)
+  if n < 2
+    return false
+  else
+    for d in 2..(n - 1)
+      if (n % d) == 0
+      return false
+    end
+  end
+ end
+  true
+end
+
+
+
+
