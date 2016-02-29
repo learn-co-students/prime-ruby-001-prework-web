607d548fcadd1f5744bc3841c4e538d8882eccab
snatow <natow.sarah@gmail.com>
2016-02-28 19:12:29 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -3,15 +3,12 @@ def prime?(number)
     number != 0
   elsif number == 1
     number != 1
-  elsif number == 101013
-    return false
   elsif number != 0 || number != 1
     for element in 2...number
-      if (number % element) != 0
-        return true
-      else 
+      if (number % element) == 0
         return false
       end
     end
+    return true
   end
 end
