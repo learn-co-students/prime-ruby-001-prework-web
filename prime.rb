941baa14e79c60759d52ee46cd8e8ccb7d112add
KielaPeoples <kielapeoples@gmail.com>
2016-03-01 01:52:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,12 +2,7 @@ def prime?(num)
   if num == 1 || num == 0 || num % 2 == 0
     return false
   else
-    numArr = (3..Math.sqrt(num)).to_a.select do
-      |number| number.odd?
-    end
-      .reject do
-        |number| number == num
-      end
+    numArr = (3..Math.sqrt(num)).to_a
     numArr.all? {|n| num % n != 0}
   end
 end
