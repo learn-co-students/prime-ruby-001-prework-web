7d74d5050f096ccf8e21e9cb401b05cc5a01fd05
robintully <mr.robintully@gmail.com>
2016-03-04 07:06:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  prime = true
+  x = 2
+
+  if num <= 2
+    prime = false
+  end
+
+  while (x <=num/2)
+    if  (num % x == 0)
+      prime = false
+      break
+    end
+    x+=1
+  end
+ return prime
+end
