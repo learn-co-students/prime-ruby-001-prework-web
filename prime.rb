2451fbeb45aa40249f483c424c00331bb55ce873
asackofwheat <asackofwheat@gmail.com>
2016-03-04 17:45:00 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  array = (1..int).to_a
+  prime_array = []
+  array.each do |x|
+    if int % x == 0
+      prime_array << x
+    end
+  end
+  if prime_array == [1, int]
+    true
+  else
+    false
+  end
+end
