802c223de90d3c51e3b2277529be38ebdd1b87ce
chriswigington <chriswigington@gmail.com>
2016-03-05 03:30:01 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  #variables
+  divisor = 2
+  #code
+  if num <= 1
+    return false
+  else
+    while divisor <= num / 2
+      if num % divisor == 0
+        return false
+      end
+      divisor += 1
+    end
+  end
+  return true
+end
