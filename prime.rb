3f736b3f7d34dc819841b81338bd87243107cae4
dougcrisona <dougcrisona@gmail.com>
2016-03-06 12:13:44 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2  
+    return false
+  end    
+  (2...integer).to_a.each do |num| 
+    if integer % num == 0
+      return false
+    end
+  end
+  return true 
+end
+
+
+
