47031e41008a331a59bc27b165a3098e9c495716
nicholas-taub <nicholas.taub@gmail.com>
2016-03-08 19:39:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+
+
+def prime?(integer)
+    if integer < 2
+      return false
+    end
+  for num in 2..(integer - 1)
+    if (integer % num) == 0
+      return false
+    end
+  end
+  true
+end
+
+
