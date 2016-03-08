232ae76f0f9f80abcff6847948f9c66dc844d4d3
joshgreen1227 <joshgreen1227@gmail.com>
2016-03-07 23:54:25 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+  test_array = 2..(integer**0.5).ceil
+  test_array.each do |factor|
+    if integer % factor == 0
+      return false
+    end
+  end
+  return true
+end
+
