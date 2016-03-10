14ed529e8ef5bfd49fb9acfa6af9c2ea17a575c9
KevinDTran <kevin@zipmark.com>
2016-03-10 09:01:58 -0600

    Completed

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  if number == 0 or number == 1
+    return false
+  elsif number == 3
+    return true
+  elsif number % 2 == 0
+    return false
+  elsif number % 3 == 0
+    return false
+  else return true
+  end
+end
