5afcfc656fc30e3fb2f9e89b1e6ca960ef62724d
ezaman <ehsanmzaman@gmail.com>
2016-03-10 13:59:06 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(integer)
+  if integer == 0
+    return false
+   elsif integer == 1
+    return false 
+   elsif (2...integer).all? { |i| integer % i != 0}
+    return true
+   else return false
+  end
+end
