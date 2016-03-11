03de6e831ae8bcfd7d5cebc90c74f215d6244a50
Tal <talgoldfus@gmail.com>
2016-03-11 19:22:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+
+def prime?(n)
+
+  if n>1
+     for div in 2..(n - 1) do
+       if (n % div) == 0
+         return false
+       end
+     end
+  else return false
+ end
+
+true
+end
+
--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -6,13 +6,15 @@ describe "prime?" do
     expect(prime?(3)).to be(true)
     expect(prime?(11)).to be(true)
     expect(prime?(105557)).to be(true)
+
   end
 
   it 'returns false for non-prime numbers' do
-    expect(prime?(0)).to be(false)
+expect(prime?(0)).to be(false)
     expect(prime?(1)).to be(false)
     expect(prime?(4)).to be(false)
     expect(prime?(40)).to be(false)
-    expect(prime?(101013)).to be(false)
+ expect(prime?(101013)).to be(false)
+
   end
 end
