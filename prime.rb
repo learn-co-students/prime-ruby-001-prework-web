00e02173db827f735e9be600adaf20d0e47fc766
canhtrinh <canh.trinh@gmail.com>
2016-03-10 19:43:19 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  if number ==0 || number ==1
+    return false
+  end
+
+  for i in 2..number-1
+    if (number % i) == 0
+      return false
+    end
+  end
+
+  true
+end
