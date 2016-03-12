23a8b01b9ffb64b074a1fb941e27645bf63d8849
Reed Dunkle <reeddunkle@gmail.com>
2016-03-12 22:44:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  range_of_numbers = (2..num).to_a
+  if (num > 1)
+    is_prime = true
+  else
+    is_prime = false
+  end
+
+  range_of_numbers.each do |i|
+    if (num % i == 0 && num != i)
+      is_prime = false
+    end
+  end
+  is_prime
+end
