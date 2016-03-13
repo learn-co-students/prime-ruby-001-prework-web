098c164b7a927d17e94911405614e8033fb74860
kallaugher <alicekallaugher@gmail.com>
2016-03-12 20:17:42 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+def prime?(n)
+  
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  elsif n % 2  == 0 || n % 3 == 0
+    return false
+  else
+
+    i = 5
+
+    while (i * i) < n
+      if n % i == 0 || n % (i + 2) == 0
+        return false
+      else
+        i += 6
+      end
+    end
+
+  return true
+  end
+
+end
+
+
+
