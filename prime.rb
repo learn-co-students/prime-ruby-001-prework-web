8f4c8ac12b3a6fe1d309f32cd61a39694edfa3da
daniellasheva <daniellasheva@gmail.com>
2016-03-14 16:57:20 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime? (n)
+        if n==0
+            return false
+        elsif n==1
+            return false
+        elsif n == 2
+            return true
+        elsif n == 3
+            return true
+        elsif n % 2 == 0
+            return false
+        elsif n % 3 == 0
+            return false
+        elsif n % 5 == 0
+            return false
+        elsif n% 7 == 0
+            return false
+        else
+        return true
+    end
+end
