2629e0ebdc8319e861514a9da85af4ec51b0747e
davidtsai130 <davidtsai130@gmail.com>
2016-03-15 04:51:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+    for x in 2..(integer - 1)
+      if integer % x == 0
+        return false
+      end
+    end
+    return true
+end
