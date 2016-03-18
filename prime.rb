5b54e73331eb4a8e2072e23f5256a6846bb1ddbe
BriOD <briodonoghue@gmail.com>
2016-03-18 15:41:08 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+def prime?(num)
+    isPrime = true
+    
+    if num == 0 || num == 1
+        isPrime = false
+    end
+    
+    
+    if num < 2
+        return false
+        end
+    (2..num-1).each do |num2|
+        if (num % num2 == 0)
+            isPrime = false
+            break
+        else
+        isPrime = true
+            
+            end
+     
+    end
+    isPrime
+    
+end
