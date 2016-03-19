7dc56bd68c304018c96922ebe0bc4dc02ded83a2
WerkerM <michael.handwerker@gmail.com>
2016-03-19 01:21:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 def prime?(num)
   value = true
-
-    if num < 2
+  array = (2..num-1).to_a
+  
+  if num < 2
         value = false
     end
-
-    (2..num-1).each do |num2|
+  
+    array.each do |num2|
         if num % num2 == 0
             value = false
             break
