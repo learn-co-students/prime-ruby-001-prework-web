9c620bb128a57855d6dd7d1bf224dc2aa4dfb1b7
WerkerM <michael.handwerker@gmail.com>
2016-03-19 01:11:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(num)
+  value = true
+
+    if num < 2
+        value = false
+    end
+
+    (2..num-1).each do |num2|
+        if num % num2 == 0
+            value = false
+            break
+        else
+            value = true
+        end
+    end
+
+    value
+end
