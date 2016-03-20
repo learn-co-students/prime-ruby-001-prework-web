649fcf66b884b800abb19c85b6fb33f374997392
RHOPRHH <rhoprhh@gmail.com>
2016-03-20 19:01:16 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n)
+  is_prime = true
+  if n <= 1
+    is_prime = false
+  end
+  for i in 2...n
+    if n % i == 0
+      is_prime = false
+    end
+  end
+  is_prime
+end
