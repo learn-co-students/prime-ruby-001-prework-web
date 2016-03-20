dcd772611376b622a52c5bac89a7cee38d457fb4
amrobayo <arobayo@wesleyan.edu>
2016-03-20 15:58:00 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+def prime?(num)
+isPrime= true
+divisor =2
+if num <2 
+  isPrime=false
+
+elsif num>2 
+  while(divisor <= Math.sqrt(num) && isPrime) do
+    if(num%divisor ==0)
+      isPrime= false
+    end
+    divisor+=1 
+  end
+
+end
+
+isPrime
+end
