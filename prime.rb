02ef612c6c88efa6737222109859a9360404b902
shalin1 <shalin.scupham@gmail.com>
2016-03-21 14:59:24 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+
+def prime?(num)
+	if num <= 1
+		return false 
+	end
+ 		
+  (2..Math.sqrt(num)).each { |i|
+  	if num % i == 0 
+  		return false 
+  	end
+  } 
+  return true
+
+end
+
+#puts "test any number for primality with the prime? method!"
+#puts "what number do you want to test for primeality?"
+#num = gets.to_i
+#if prime?(num) == true
+#	puts "that number is totally prime!"
+#end
+#if prime?(num) == false
+#	puts "that number is totally not prime!"
+#end
+
