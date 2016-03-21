543d909d9f706b2fc13a1f90b4eb15b64e888d9b
jacobshiff <shiffjacob@gmail.com>
2016-03-21 14:14:03 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime? (number)
+  if number == 1
+    false
+  elsif number == 2
+    true
+  else 
+    divisors = 0
+    for i in 2..number
+      if number % i == 0
+        divisors += 1
+      end
+    end
+    if divisors == 1
+      true
+    else
+      false
+    end
+  end
+end 
+
+puts prime?(2)
+puts prime?(5)
+puts prime?(6)
+puts prime?(123)
+puts prime?(125)
