cb16a456246ffb67e15ba3f00bb166973e479d2a
mlombard82 <mlombard82@gmail.com>
2016-03-25 15:47:34 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(i)
+     num = 2
+     while num < i || i == 0 || i == 1
+         test = i%num == 0
+         if test || i == 0 || i ==1
+             return false
+         end
+ num += 1
+ end
+ true
+end
