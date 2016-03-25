565e947d12015ef0c98cc72a67d3ba1c1b2fc743
colbyschulz <colbyschulz@gmail.com>
2016-03-25 15:03:33 -0500

    revised for negative numbers

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(i)
- num = 2
- while num < i || i == 0 || i == 1
-     test = i%num == 0
-     if test || i == 0 || i ==1
-         return false
-     end
+     num = 2
+     while num < i || i == 0 || i == 1 || i < 0
+         test = i%num == 0
+         if test || i == 0 || i == 1 || i < 0
+             return false
+         end
  num += 1
  end
  true
