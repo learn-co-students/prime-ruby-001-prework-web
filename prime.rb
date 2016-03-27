30b63571a878d4dbbdb17fcdb2514fc89e741a2e
kparekh01 <kparekh01@gmail.com>
2016-03-27 04:43:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+   return nil if num < 0
+   return false if num == 1 || num == 0
+   for each_num in 2...num
+   if num % each_num == 0
+      return false
+   end
+   end
+   true
+end
