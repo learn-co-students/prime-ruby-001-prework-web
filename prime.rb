27802dece65872fcb9b4531ded1e3375032fee83
mlombard82 <mlombard82@gmail.com>
2016-03-31 11:55:34 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,14 @@
 # Add  code here!
-def prime?(i)
-     num = 2
-     while num < i || i == 0 || i == 1
-         test = i%num == 0
-         if test || i == 0 || i ==1
-             return false
-         end
- num += 1
- end
- true
+def prime? (int)
+  if int < 2
+    return false
+  end
+
+  for num in 2..(int/2)
+    if (int % num) == 0
+      return false
+    end
+  end
+
+  true
 end
