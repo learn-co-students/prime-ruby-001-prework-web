da665109223dc5f8dd709032db0cf76b116073f6
ToddRizley <todd.rizley@gmail.com>
2016-04-01 22:06:20 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+  if num == 0
+    false
+  elsif num == 1
+    false
+  elsif num == 2
+    true
+  else
+  is_prime = true
+  for i in 2...num do
+    #puts "#{i}"
+    if num % i == 0
+      is_prime = false
+      break
+    end
+  end
+  is_prime
+end
+end
