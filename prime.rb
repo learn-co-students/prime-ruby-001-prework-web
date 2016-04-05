59347a963803fa4d3549074b46341e3c1b5e6aa4
BettinaP <bettinaprophete@gmail.com>
2016-04-04 22:48:19 -0400

    prime 2

--- prime.rb
+++ prime.rb
@@ -3,14 +3,15 @@ def sieve(max)
   primes[0] = primes [1] = nil
   counter = 0
   primes.each do |x|
-    next unless x
-    break if x*x > max
-    counter +=1
-    (x*x).step(max,x) { |m| primes[m] = nil}
-  end
+  next unless x
+  break if x*x > max
+  counter +=1
+  (x*x).step(max,x) { |m| primes[m] = nil}
+
+end
   primes.compact
 end
-    
+
 def prime?(number)
 sieve(number).include?(number)
 end
