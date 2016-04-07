1a20b3f1fbbd0c98a9591c6d389eea05b879dbca
jin <jinzheng87@gmail.com>
2016-04-06 23:51:12 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(number)
+    if number < 2
+      return false
+    end
+    
+    (2...number).each do |n|
+      if number % n == 0
+        return false
+      end
+    end
+    return true
+end
