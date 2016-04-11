3c78454549b90d156f69cac65b396ab0d229c410
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-04-11 16:16:23 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
+def prime?(arg)
+
+idx = 2
+x = 0
+
+if arg == 0 || arg == 1
+  x += 1
+end
+
+while x == 0 && idx < arg
+if arg % idx == 0
+x += 1
+end
+idx += 1
+end
+
+if x > 0
+  return false
+else 
+  return true
+end 
+
+end
+
 # Add  code here!
