0efcdbcd8b588d62bbe4bc6a741df376dc93db7f
blessedsoy <unmigong@gmail.com>
2016-04-12 01:37:55 -0400

    Implementing Prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
+# require 'pry'
+
+def prime?(number)
+  # binding.pry
+  if number < 2
+   return false
+  else 
+    for i in 2..number - 1 do 
+      if number % i == 0
+        return false
+      end
+    end
+  end
+  return true 
+end
+
+
 # Add  code here!
