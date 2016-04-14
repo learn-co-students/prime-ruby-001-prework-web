42842bf86edf08c0b043578370daf67f372794c7
jjrajani <jjrajani@gmail.com>
2016-04-14 14:10:57 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  count = 0
+  for i in (1..num) do 
+    if num % i == 0
+      count +=1
+    end
+  end
+    if count != 2
+      return false
+    else
+      return true
+    end
+
+end
