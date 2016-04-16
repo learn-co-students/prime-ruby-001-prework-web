5c54b12089284e585a1fdc83ec23ac6a3efe5556
abcheromar <abcheromar@gmail.com>
2016-04-16 01:48:46 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,9 @@
 def prime?(number)
-
   return false if number < 2
   (2..Math.sqrt(number)).each do |num|
-  return false if number % num == 0 
+    return false if (number % num).zero?
   end 
-
   true
-
 end
 
 
