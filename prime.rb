9e6996954d93dc25e87c29f9aefcae62ae0d0fc9
jeffpwang91 <jeffpwang91@gmail.com>
2016-04-17 18:53:48 +0000

    nitrous-backup at Sun Apr 17 18:53:48 UTC 2016

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
-# Add  code here!
+# Add  code here! A prime number is greater than 1 and can only be divisible by 1 and itself.
 
 def prime?(num)
   if num <= 1
     return false
   end
-  Math.sqrt(num).to_i.downto(2).each do |i|
+  Math.sqrt(num).to_i.downto(2).each do |i| #test to see if square root of number down to 2 is divisible by number
     if num % i == 0
-      return false
+      return false #if divisible by a number between 2 and sqrt of number, then it is not prime
     end
   end
   true
