5269edd9b90e954e65955b8466b7442703275e65
wvazquez <vazquezwilmer@gmail.com>
2016-04-20 15:55:54 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  return false if number == 0 || number == 1
+  for num in 2...number
+    if(number % num == 0)
+      return false
+    end
+  end
+  return true
+end
