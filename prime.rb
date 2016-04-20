ce56cd5ecb32d5d273f76dba43754c7a53c99e04
acodemonkey <bnparsons@gmail.com>
2016-04-19 21:36:53 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+  biggest_divisor = 1
+  for i in 2...number
+    if (number % i) == 0
+      biggest_divisor = i
+    end
+  end
+  biggest_divisor == 1 && number > 1 ? true : false
+end
