cc8724afae3db35822fa0d8b8c86cc7905db460a
quarthex15 <andrew.klein.j@gmail.com>
2016-04-23 11:01:36 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(num)
+  if num == 0 || num == 1
+    false
+  else
+    test_range = 2..100
+    bool_range = []
+
+    test_range.each do |x|
+      if num%x != 0 || num == x
+        bool_range << true 
+      else
+        bool_range << false
+      end
+    end
+    
+    !bool_range.include?(false)
+  end
+end
+
+
