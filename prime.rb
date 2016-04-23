4b09147a368dd0b8e8fbeaa0100b98cd0f82e7b6
franklinbrooks <gutterheart@hotmail.com>
2016-04-23 12:52:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime? (number)
+    if number <= 1
+        false
+    elsif number == 2
+        true
+    else
+        (2..number/2).none? { |i| number % i == 0}
+    end
+end
