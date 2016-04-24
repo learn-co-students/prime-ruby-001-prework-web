01fedfa2549fbbb052a78698678f9613ae51fc93
thedailyephemera <thedailyephemera@gmail.com>
2016-04-24 11:59:41 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+	if number <= 1
+		return false
+	elsif number <= 3
+		return true
+	else
+		(2..number/2).none?{|num| number % num == 0} ? true : false
+	end
+end
