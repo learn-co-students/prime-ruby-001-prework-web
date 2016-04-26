bcd9dacbe08d568a98700a106038ff9b16d9b319
christinechoi <smi77y@gmail.com>
2016-04-25 21:14:25 -0400

    completed lab

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  (2...num).each do |int|
+    if num % int == 0
+      return false
+    end
+  end
+
+  else
+    return true
+  end
+
+
+#sqrt = Math.sqrt(num)
+ # range = (2..sqrt).to_a
+#
+ # range.each do |x|
+  #  divisor = num % x.to_i 
+   #  return true
+  #  else
+  #    return false 
+  #  end
+  
+
+ 
