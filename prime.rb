b7d574c837482de10ac58ac65ef5ff06aee322b5
Gilad Barkan <giladbarkan@live.com>
2016-04-28 21:38:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(n)
+  if n == 0 || n == 1
+    return false
+
+  elsif n == 2
+    return true
+
+  elsif n % 2 == 0
+    return false
+
+  end
+
+  i = 3
+
+  while i*i <= n
+    if n % i == 0
+      return false
+    end
+    i += 2
+  end
+
+  return true
+end
