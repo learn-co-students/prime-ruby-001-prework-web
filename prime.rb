a2e56e34909907706f94b08b94743c55aab3e1c6
Max E <etiemaxo@gmail.com>
2016-04-28 03:41:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  answer = true
+  if number < 2
+    answer = false
+  end
+
+sqrtX = (2..Math.sqrt(number)).to_a
+  sqrtX.each do |num|
+   if number % num == 0
+    answer = false
+  end
+end
+  answer
+end
