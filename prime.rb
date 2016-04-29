f198a9ec721c70cb35ab4569e9eb28e7d5de0f13
hailuhr <hredleaf@gmail.com>
2016-04-29 01:49:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(num)
+  if num == 0 || num == 1
+    return false
+  end
+
+  idx = 2
+  while idx < num
+    if num % idx == 0
+      return false
+    end
+    idx += 1
+  end
+  return true
+end
