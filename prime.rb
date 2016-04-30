1d8e5f763d2d5ff41f2c1edea0d198361bf39f78
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-04-30 18:04:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+
+#defining gcd method
+def gcd(x,y)
+  while y > 0
+    tmp = x
+    x = y
+    y = tmp % y
+  end
+  x
+end
+
+
+def prime?(num)
+  t = 2
+  if ( num == 0 || num == 1 )
+    return false
+  end
+  while t <= num/2
+    if ( gcd(t,num) != 1)
+      return false
+    end
+    t += 1
+  end
+    return true
+end
