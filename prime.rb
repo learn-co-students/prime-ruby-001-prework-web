63edaa54f4533e788a9e0a58349934f75c1a381a
David Wexler <dwexler@gmail.com>
2016-05-08 03:48:01 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
+def prime? (n)
+  if n < 2
+    return false
+  elsif n == 3
+    return true
+  end
+  array = []
+  for i in (2..(n/2)) do
+    array.push(i)
+  end
+  array.each do |i|
+    if n % i == 0
+      return false
+    end
+  end
+  return true
+end
+
+
 # Add  code here!
