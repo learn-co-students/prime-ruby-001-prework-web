b6f655700073be815c3b105f8ffaad4265e85509
paige11 <ellis.paige.v@gmail.com>
2016-05-08 13:00:54 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 def prime?(n)
-  for d in 2..(n - 1)
-    if (n % d) == 0
+  if n < 2
+    return false
+  end
+  array = (2..(n-1)).to_a
+  array.each do |x|
+    if (n % x) == 0
       return false
     end
   end
-    if n < 2
-      return false
-    end
   true
 end
