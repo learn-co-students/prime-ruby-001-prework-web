968a94731bbf7c6a12ce9a1dcb0e081dfca139fb
FizaBibi <fizabibi89@gmail.com>
2016-05-09 20:14:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(int)
+  if int <= 1
+    return false
+  end
+    start = 2
+  while (start < int)
+    if (int % start == 0)
+      return false
+      break
+    end
+    start += 1
+  end
+  return true
+
+end
