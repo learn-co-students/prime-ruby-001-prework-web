4c2b73cf3187e49a7c7fadb7d86bca15445a6f02
DG4227 <danielhgorman@gmail.com>
2016-05-10 23:20:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+
+  divisors = (2..((num / 2) + 1)).to_a
+
+  if num.between?(0, 2)
+    return false
+  end
+
+  divisors.each do |i|
+    if (num % i) == 0
+      return false
+    end
+  end
+  return true
+end
