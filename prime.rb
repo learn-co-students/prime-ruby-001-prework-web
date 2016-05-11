0a288765d11ebef0765b0f2c04328e01f95abb31
Kevin Hernon <khernon1@gmail.com>
2016-05-11 13:27:31 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(n)
+
+if n <= 2
+  if n == 2
+    return true
+  else
+    return false
+  end
+elsif n % 2 == 0
+    return false
+end
+
+i = 3
+while i <= Math.sqrt(n)
+  if n % i == 0
+    return false
+  end
+i += 1
+end
+
+return true
+end
