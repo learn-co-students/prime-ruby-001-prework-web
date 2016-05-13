6ee9cbf6826ec1d0763188142388dc3968614f40
tonymaibox <tonymaibox@yahoo.com>
2016-05-13 21:40:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num <= 3
+    return true
+  else
+    i = 2
+      while i <= (num/2)
+        if num % i == 0
+          return false
+        end
+        i += 1
+      end
+  end
+  return true
+end
