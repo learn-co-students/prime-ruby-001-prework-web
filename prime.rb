56e5e03ffa172c1113712c3f501381d956cedb22
ericastevens <ericastevens99@gmail.com>
2016-05-13 22:34:19 +0000

    nitrous-backup at Fri May 13 22:34:19 UTC 2016

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+int = 0
+
+def prime?(int)
+  (0..106000).each do |x|
+    x+=1
+    remainder = x % 2
+       case
+        when remainder == 0
+          return remainder == 0
+        when remainder == 1
+          return remainder != 0
+        when x == 0 || x == 1
+          return false
+        end
+  end
+end
+
+prime?(int)
