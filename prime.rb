5d48711df17135bbfde2a9507ce110db0cc28f84
Salome Braganza <salomekbg@gmail.com>
2016-05-16 12:58:34 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+  check = false
+  count = 2
+
+  while count < number && number > 2
+    if number % count != 0
+      check = true
+      count += 1
+    else
+      check = false
+      break
+    end
+  end
+
+  return check
+end
+
+prime?(101013)
