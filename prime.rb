34ae0bbb282c5b03ea42637eab7b9ccef94df7f6
Thomas Marren <tmarren2090@gmail.com>
2016-05-16 02:03:35 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+def prime?(num)
+
+  return false if num == 0 || num == 1
+  return true if num == 2
+
+  first,last = 2, num - 1
+  range = first..last
+
+  range.each do |x|
+    return false if num % x == 0
+  end
+
+  return true
+
+end
