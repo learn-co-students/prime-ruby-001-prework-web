b430e8e0df7241e30fe236484e2dbb4efc1f3cef
kaptiin-tuekhov <nedoumok99@gmail.com>
2016-05-18 22:21:09 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  remainder_array = []
+  for i in 2..number-1
+    remainder_array << number % i
+  end
+  remainder_array.each do |remainder|
+    if remainder == 0
+      return false
+    end
+  end
+  if number == 0 || number == 1
+    return false
+  elsif number % number == 0 && number % 1 == 0
+    return true
+  end
+end
