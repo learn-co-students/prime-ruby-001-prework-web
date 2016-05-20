e164811832282e88d5c5a782cca3ea61b3e449ff
jinstrider2000 <efrainperezjr@live.com>
2016-05-19 22:37:20 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  
+  if(number.integer?)
+    if number <= 1
+      false
+    else
+    !(2..number-1).any? { |e| number % e == 0 }
+    end
+  else
+    false
+  end
+end
