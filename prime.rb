a75e9065744e368596c307e9844796c8fc293d7a
Salome Braganza <salomekbg@gmail.com>
2016-05-24 14:19:40 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,34 +1,14 @@
-=begin
-def prime?(number)
-  check = false
-  count = 2
-
-  while count < number && number > 2
-    if number % count != 0
-      check = true
-      count += 1
-    else
-      check = false
-      break
-    end
-  end
-
-  return check
-end
-=end
-
-def prime?(number)
-  range = (2..(number - 1)).to_a
-  check = true
-  if number < 2
-    check = false
-  else
-    range.each do |x|
-      if number % x == 0
+def prime?(num)
+check = true
+  if num > 1
+    for a in (2...num)
+      if num % a == 0
         check = false
         break
       end
     end
+  else
+    check = false
   end
-   puts check
+  check
 end
