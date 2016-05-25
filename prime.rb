fefa856aa96ba815a6f407aad8a460fa60db6663
Raycent Tan <ltan2@babson.edu>
2016-05-25 09:04:08 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  return false if number < 2 || number % 2 == 0
+
+  factor = 3
+  while number / 2 > factor
+    return false if number % factor == 0
+    factor += 2
+  end
+
+  true
+end
