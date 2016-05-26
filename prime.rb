9eaf51494ab1fc99850a40f97d889043ed694bf1
Dillon McRoberts <dillon.mcroberts@gmail.com>
2016-05-26 10:54:46 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(int)
+  range = (2...int).to_a
+
+  if int == 0
+    return false
+  elsif int == 1
+    return false
+  end
+
+  range.each do |num|
+    if int % num == 0
+      return false
+    end
+  end
+    return true
+end
