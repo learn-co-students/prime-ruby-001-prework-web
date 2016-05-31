2dfe3558d9f3a7bb0eac0a7718f9f5b1c301eff2
Leeor Baskin <lbaskin@gmail.com>
2016-05-30 22:19:00 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  if num == 1
+    return false
+  elsif num % 2 == 0
+    return false
+  end
+  for i in (3..(Math.sqrt(num)+1)).step(2) do
+    if num % i == 0
+      return false
+    end
+  end
+  return true
+end
