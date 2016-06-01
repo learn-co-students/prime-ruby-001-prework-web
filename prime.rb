efc8c5cf71e72191fd2dae58fa8b53e228fbeea3
Salome Braganza <salomekbg@gmail.com>
2016-06-01 12:52:43 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,14 +1,17 @@
 def prime?(num)
-check = true
-  if num > 1
-    for a in (2...num)
-      if num % a == 0
-        check = false
-        break
-      end
+  numbers = (2...num).to_a
+  check = false
+  numbers.each do |item|
+    if num < 2
+      check = false
+    elsif num == 2
+      check = true
+    elsif num % item != 0
+      check = true
+    elsif num % item == 0
+      check = false
+      break
     end
-  else
-    check = false
   end
   check
 end
