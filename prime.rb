6b7740ad6bb33fdadcf451964d23009c7db54ac7
Tami <catquarks@gmail.com>
2016-06-05 22:57:27 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+#require 'benchmark'
+
+def prime?(num)
+	if num == 0 || num == 1
+		return false
+	else
+		i = 2
+		while i <= num
+				if num % i == 0 && i == num
+					return true
+				elsif num % i == 0 && i != num
+					return false
+				else
+					i +=1
+				end
+		end
+	end
+end
+
+#puts Benchmark.measure {prime?(100)}
