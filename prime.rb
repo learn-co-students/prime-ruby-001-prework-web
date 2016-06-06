6b7e3e98b01833bf67557b0b55e503262f17f052
SharkBroPhD <troychryssos@gmail.com>
2016-06-06 17:32:40 -0400

    Built a prime determining method.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(x)
+  return false if x == 1
+  return false if x <= 0
+  return true if x == 2
+  i = 2
+  while x > i
+    return false if x % i == 0
+    i+=1
+  end
+  true
+end
