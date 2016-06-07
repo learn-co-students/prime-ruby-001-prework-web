5730da7b94061abad1ead3c5135629fe9dce8a23
pkolodgy <pkolodgy@gmail.com>
2016-06-07 15:31:00 -0400

    completed prime.rb

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(num)
+  num = num.to_i
+
+  if num <= 1
+    puts num
+    return false
+  elsif num <=3
+    return true
+  elsif num % 2 == 0 || num % 3 == 0
+    return false
+  end
+
+  i = 5
+  while (i * i) <= num
+    if num % i == 0 || num % (i + 2) == 0
+      return false
+    end
+    i += 6
+  end
+
+  return true
+end
