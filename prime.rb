bfc409d55568296fafb01de26ed88f430c0d2aa6
Stefanie Charles <apifanny@aol.com>
2016-06-08 20:47:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
+def  prime? (num)
+  if (num==0 || num==1)
+      return false
+    end
+    for x in 2..num-1
+      if (num%x ==0)
+        return false
+      end
+    end
+    return true
+  end
 # Add  code here!
