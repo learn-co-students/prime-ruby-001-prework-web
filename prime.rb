f1d3b47b78bb611893978bf27142349a6cc86517
Hafizur Rahman <hafizur.rahmanc@gmail.com>
2016-06-11 12:18:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  count = 2
+  if (number < 2)
+    return false
+  elsif (number == 2 || number == 3)
+    return true
+  else
+    while count < number do
+      if (number % count) == 0
+        return false
+      end
+      count += 1
+    end
+    return true
+  end
+end
