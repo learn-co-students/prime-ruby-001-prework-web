e6ceec3cd434e33bddeb54abcc994d9d7134bab7
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-06-11 00:54:46 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(integer)
+  answer = true
+  if integer > 2
+    for x in 2..(integer - 1)
+      if integer % x == 0
+        answer = false
+        break
+      end
+    end
+  elsif integer == 2
+    answer = true
+  else
+    answer = false
+  end
+  return answer
+end
