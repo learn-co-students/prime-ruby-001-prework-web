f5d9714f9e26679519265e757637f744c23ee0df
Engin Tandogan <pmailpool2@yahoo.com>
2016-06-20 21:31:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime?(n)
+
+  if n < 2
+    return false
+  end
+  if n == 2
+    return true
+  end
+  # get smaller numbers to be tried for division, [2,..., n-1]
+  num_arr = []
+  i = 2
+  while i < n
+    num_arr.push(i)
+    i += 1
+  end
+
+  num_arr.each do |val|
+    if n % val == 0 # means it has a factor other than itself and 1
+      return false
+    end
+  end
+
+  return true
+
+end
