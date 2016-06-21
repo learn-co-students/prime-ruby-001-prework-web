17a3418c719a79dcedbbfb3692a5c64b8ef26289
Beth Olmo <betholmo13@gmail.com>
2016-06-21 21:47:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+  if n < 2
+    return false
+  end
+
+  numbers = Array(2...n)
+
+  numbers.each do |int|
+    if n%int == 0
+      return false
+    end
+  end
+  return true
+end
