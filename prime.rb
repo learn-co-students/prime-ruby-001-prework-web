45edf22b6ae54b92d354984225b51eb1e1bfcedc
alexanderspace100 <alexanderspace100@gmail.com>
2016-06-21 18:58:45 +0300

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+	  if num == 0 || num == 1
+    false
+  else
+    x = (2...num).to_a
+    x.each do |i|
+      try = num % i
+      if try == 0
+        return false
+      end
+    end
+    true
+  end
+end
