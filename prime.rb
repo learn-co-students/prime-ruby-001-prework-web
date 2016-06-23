a5bf39e33f658f40b327ea10c537e6e00075ad3c
Daniel Toma <dontcallmeshirley80@gmail.com>
2016-06-23 16:42:53 -0400

    prime rb

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
 # Add  code here!
+def prime?(int)
+  if int == 1
+    return false
+  end
+
+  if int == 2
+    return true
+  end
+
+  if int % 2 == 0
+    return false
+  end
+
+  if int % 2 != 0
+    x = 3
+    while x <= int/2 do
+      if int % x == 0
+        return false
+      else
+        x += 2
+      end
+    end
+    return true
+  end
+
+end
