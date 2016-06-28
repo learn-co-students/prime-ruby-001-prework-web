e7d3b73d74ed5ddbe5e518a308f35ecb7b17979b
jwuetcher <jwuetcher@mica.edu>
2016-06-28 18:07:06 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(prime)
+  if prime < 2
+    return false
+  elsif 
+    (2...prime).each do |x|
+      if prime % x ==0
+        return false
+      end
+    end
+  return true
+  end
+end
