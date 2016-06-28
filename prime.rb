a377ff9442b46bf3cdee3bbc19ace48bcd245ad8
Daniel Ness <danieljohnness@yahoo.com>
2016-06-28 18:07:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+n = 4
+def prime? (n)
+  array = (2...n).to_a
+ answer = false
+  if n <= 1
+    return false
+  else
+    array.each do |x|
+    if n % x == 0
+      break answer = false
+    else
+      answer = true
+    end
+   end
+  end
+  answer
+ end
