23d0a8975794023c3c7b27f28db262e6fba86810
Jacob Edgerly-Dowd <dowdje89@gmail.com>
2016-07-06 03:57:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+  if number == 3 || number == 2
+    prime = true
+  elsif number == 0 || number == 1
+    prime = false
+  else
+    factor = 2
+  while factor <= number/2
+    if number % factor == 0
+      prime = false
+      factor = number
+    else
+      prime = true
+      factor += 1
+    end
+  end
+end
+prime
+  end
