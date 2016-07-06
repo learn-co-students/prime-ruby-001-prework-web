acaf3ea957fc6b103f5cf491016088f6b3e2eafe
adam2222 <adam222@comcast.net>
2016-07-06 16:14:40 -0400

    refactored

--- prime.rb
+++ prime.rb
@@ -1,42 +1,9 @@
 
 def prime?(integer)
+  return false if integer <= 1
+  return true if integer == 2
 
-  if (integer == 0) # Had issues with OR (integer = 0 | integer = 1)
-    false
+  (2...integer).each { |factor| return false if integer % factor == 0 }
 
-  elsif (integer == 1) 
-  false
-
-  elsif integer == 2 
-    true
-
-  elsif integer == 3
-    true
-  
-  elsif integer == 4
-    false
-
-  elsif integer == 5
-    true
-
-  else
-
-    array = (5..integer - 1).to_a  
-    falsearray = []
-    
-    array.each do |i|
-      if integer % i == 0
-          falsearray << "F" # Had issues with return true/false and with scope (changing a variable outside the loop)
-          break 
-      end
-    end
-
-    if falsearray[0] == "F"
-       false
-    else
-       true
-    end 
-
-  end
-
+  true
 end
