b0f00a80774e05eaaa3fb5983853bc8a6bb9c2aa
Jason Arnold <jason.arnold.00@gmail.com>
2016-07-09 15:33:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  if num <= 0 ||  num == 1
+    return false
+  else
+    arr = (2..num-1).to_a.reverse
+  end
+  arr.each do |divisor|
+    if num % divisor == 0
+      return false
+    end
+  end
+  return true
+end
