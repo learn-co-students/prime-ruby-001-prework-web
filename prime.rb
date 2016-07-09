0ba2250c48e797bc48d903b7c5034da1239f17ef
Janelle Rosario <janelle.rosario@gmail.com>
2016-07-09 03:21:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime?(number_tested)
+  return false if number_tested < 2
+  (2..Math.sqrt(number_tested)).each do |num|
+  return false if number_tested % num == 0
+  end
+  true
+  end
