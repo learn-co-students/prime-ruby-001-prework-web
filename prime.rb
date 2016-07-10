199485147b210f7ac0ecee92041a033a33bcbfa0
chrismicle <cmcollura@gmail.com>
2016-07-10 16:52:31 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  else 
+    (2..num).each do |i|
+      if num % i == 0 && i < num
+        return false
+      end
+    end
+    return true
+  end
+end
+
