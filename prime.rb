161e8acf3f8451b74848cca20a63a96182e1ab56
Robert Domingo <moondig@gmail.com>
2016-07-10 20:09:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+
+  idx = 2
+  while idx < number
+    if number % idx == 0
+      return false
+    end
+    idx += 1
+  end
+  return true
+end
