6f8c6def170c216e7268b7173ec57326464df944
smcarrera <stephanie.carrera@gmail.com>
2016-07-13 12:41:17 -0400

    is prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(int)
+  test_array = (2..Math.sqrt(int).round).to_a
+
+  if int <=1
+    false
+  else
+      for i in 0..test_array.length-1
+        check = int%test_array[i]
+            if check == 0
+              #puts "not prime"
+              return false
+              break
+            end
+        end
+    #puts "is prime"
+    true
+  end
+end# Add  code here!
