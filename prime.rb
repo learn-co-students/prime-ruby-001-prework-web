4eef646620ae8f60272d1abf9e38cb65fe20482d
colbygatte <colbygatte@gmail.com>
2016-07-13 14:58:22 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(int)
+  return false if int <= 1
+  #return false if int.class != Integer
+
+  for n in (2..int-1)
+    if(int % n == 0)
+      return false
+    end
+  end
+
+  true
+end
