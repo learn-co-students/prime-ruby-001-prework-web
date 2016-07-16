801c2d06916ac6e5cda987e8ac6c0e507e6d7071
Alice Wu <dragon3658aw@gmail.com>
2016-07-16 16:34:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+  if int == 0 || int == 1
+    return false
+  end
+
+  for x in 2..(int - 1)
+    if int % x == 0
+      return false
+    end
+  end
+  true
+end
