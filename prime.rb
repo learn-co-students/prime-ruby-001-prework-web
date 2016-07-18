9692c6710a5e6b8d7bb01b65538b7d55788c6bf4
WinnieLee <winlyw0710@gmail.com>
2016-07-17 20:35:11 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+
+def prime?(integer)
+  if integer == 2 || integer == 3
+    return true
+  elsif integer == 1 || integer % 2 == 0 || integer % 3 == 0
+    return false
+  else
+    return true
+  end
+end
