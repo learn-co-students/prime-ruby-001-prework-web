f1222d38143a0c60f5c1a01a249e1e9142c7a8c7
Charlie Spencer <crs992@gmail.com>
2016-07-19 14:46:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+
+  return false if num <= 1
+#  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+#  true
+
+  Math.sqrt(num).to_i.downto(2).each do |i|
+    return false if num % i == 0
+  end
+  true
+
+end
