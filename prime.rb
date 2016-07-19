1307acb1f681990b4eebade49311ce2de8673089
Camille Jackson <cj@mail.co.de>
2016-07-19 16:36:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  if integer > 3
+    if integer % 3 == 0 || integer % 2 == 0 || integer % 5 == 0 || integer % 7 == 0
+      false
+    else
+      true
+    end
+  elsif integer == 2 || integer == 3
+    true
+  else
+    false
+  end
+end
