338ca75b8c472a2a09e9dc75e9a4cfa4c1952230
Steffi Balarezo <balarezos@gmail.com>
2016-07-20 00:47:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime? (integer)
+  return false if integer == 0
+  return false if integer == 1 
+  (2...integer).each do |divisor|
+    if integer % divisor == 0
+     return false
+    end
+  end
+  return true
+end
