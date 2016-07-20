25434eb82d54a7de3d4400eff096e4579b1a2f67
Adam Intrator <adam.intra2r@gmail.com>
2016-07-20 13:35:37 -0400

    added optimized second prime algorithm plus benchmark

--- prime.rb
+++ prime.rb
@@ -1,4 +1,6 @@
 
+require 'benchmark'
+
 def prime?(integer)
   return false if integer <= 1
   return true if integer == 2
@@ -7,3 +9,27 @@ def prime?(integer)
 
   true
 end
+
+
+def prime_optimized?(integer)
+  return false if integer <= 1
+  return true if integer == 2
+  return false if integer % 2 == 0 || integer % 3 == 0
+
+  i = 5
+  while (i * i) <= integer
+    if integer % i == 0 || integer % (i + 1) == 0
+      return false
+    end
+    i += 6
+  end
+
+  true
+end
+
+Benchmark.bm do |benchmark|
+  benchmark.report('non_optimized') { 2.times {prime?(15485863)} }
+  benchmark.report('optimized') { 2.times {prime_optimized?(15485863)} }
+end
+
+# 160481219
