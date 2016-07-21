31a14a5213a97c995b2453c9b7ba77422837c7b6
MichelleBKlynn <michellestaton@gmail.com>
2016-07-20 23:58:28 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+
+def sieve(max)
+  primes = (0..max).to_a
+  primes[0] = primes[1] = nil
+  counter = 0
+  primes.each do |p|
+    next unless p
+    break if p*p > max
+    counter += 1
+    (p*p).step(max,p) { |m| primes[m] = nil }
+  end
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
+end
+
+def prime?(num)
+  sieve(num).include?(num)
+end
+
+# After researching the most efficient way to identify prime numbers
+# without using 'Math', I found the Sieve of Eratosthenes 
+# way via https://gist.github.com/loganhasson/8937903
