9e5d7a6746ed82d3d758ada023c631c62ce3a098
Carmen Vivas <selain27@gmail.com>
2016-07-21 09:29:30 -0400

    I think my code could be better

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)# Add  code here!
+  return false if int <= 1
+  if int % 2 == 0
+    false
+  elsif int == 3
+    true
+  elsif int % 3 == 0
+    false
+  else
+    true
+  end
+end
