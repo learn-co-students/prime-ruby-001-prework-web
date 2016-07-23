f324f29a5f22e77a648d81df4310e67fda5c2b42
craigschiff <craig.schiff@gmail.com>
2016-07-23 15:35:36 -0400

    success

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+    range = 2..num
+    match = 0
+    range.each do |x|
+        if num % x == 0
+            match += 1
+        end
+    end
+    if match == 1
+        return true
+    else
+        return false
+    end
+    end
