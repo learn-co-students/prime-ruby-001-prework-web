4ddedcf517b4e794641788c5257e6ee1667cb841
craigschiff <craig.schiff@gmail.com>
2016-07-23 16:29:06 -0400

    DONE

--- prime.rb
+++ prime.rb
@@ -1,15 +1,18 @@
 # Add  code here!
 def prime?(num)
-    range = 2..num
-    match = 0
-    range.each do |x|
-        if num % x == 0
-            match += 1
-        end
-    end
-    if match == 1
-        return true
-    else
+    if num < 2
         return false
     end
+    array = (2..(num-1)).to_a
+    new_array = []
+    array.each do |x|
+        new_array.push num%x
+        end
+    if new_array.include?(0)
+        false
+    elsif num < 2
+        false
+    else
+        true
     end
+end
