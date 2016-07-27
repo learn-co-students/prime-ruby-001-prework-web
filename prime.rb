bfd28a419b8968fdb36018859bd79ec162bb2c65
Steven Tang <5teven7ang@gmail.com>
2016-07-27 19:56:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
 # Add  code here!
+# prime via trial divison
+require 'pry'
+
+def prime?(number)
+  if number < 2
+    return false
+  end
+
+  array_counter = 1
+  prime_counter = 1
+
+  possible_factors = Array.new(number)
+
+  while array_counter <= number do
+    possible_factors[array_counter - 1] = array_counter
+    array_counter += 1
+  end
+
+  while prime_counter < number - 1 do
+    if number % possible_factors[prime_counter] == 0
+      return false
+    end
+    prime_counter += 1
+  end
+  return true
+end
