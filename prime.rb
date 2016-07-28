728e6d5d025e72e5809e4106eb2dca4a4fe9c3c5
Robert Hitt <robhitt@gmail.com>
2016-07-28 15:50:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+#give a number as an argument to return true or false if the number is prime
+def prime?(num)
+	is_prime = true
+
+  if num <= 1
+    is_prime = false
+    return is_prime
+  end
+
+	(2..num-1).each do |iterator|
+		if num % iterator == 0
+      is_prime = false
+			return is_prime
+		end
+	end
+
+  return is_prime
+  
+end
