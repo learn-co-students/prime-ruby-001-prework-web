86e617bddecf68656e1e4df6b502c1fba0562cd1
slindquist3 <slindquist3@gmail.com>
2016-07-28 11:38:51 -0400

    Deteremined whether a number is prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    # only numbers > 1 can be prime.
+    return false
+  end
+
+  idx = 2
+  while idx < number
+    if (number % idx) == 0
+      return false
+    end
+
+    idx += 1
+  end
+
+  return true
+end
