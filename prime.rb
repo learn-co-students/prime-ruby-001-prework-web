9b4d293d10be26b0f406fa1f163ba3cad7600793
Michael Wiggins <michael.wiggins@me.com>
2016-07-30 10:06:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+# you can make your own benchmark
+#http://rubylearning.com/blog/2013/06/19/how-do-i-benchmark-ruby-code/
+#this is a boolean operation - testing, is this shit true?
+#in this first line, it's calling on an operation that has been defined for you and stored already. It's like magic.
+require 'prime'
+
+def prime?(num)
+    num.prime?
+end
