c861254fe90509229100b5483d3e483c2a2999e0
krissasantos <krissa.santos31@gmail.com>
2016-07-31 16:30:49 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  range = (2...num).to_a
+  if num <= 1
+    return false
+  else
+    range.each do |x|
+      return false if num % x == 0
+    end
+  end
+  return true
+end
