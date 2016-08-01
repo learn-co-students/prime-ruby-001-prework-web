2abe41eecd8ffb758ee8ec237c40bcd3d43a4415
lydie  toussaint <lydietoussaint123@gmail.com>
2016-08-01 05:28:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(is_prime)
+  if is_prime < 0
+    return false
+  elsif is_prime == 0 || is_prime == 1
+    return false
+  elsif is_prime == 3
+    return true
+  end
+  i = 2
+  while(i < is_prime)
+    if is_prime % i == 0 || is_prime % 3 == 0
+      return false
+    else
+      return true
+    end
+    i+=1
+  end
+end
