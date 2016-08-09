6a230f21a19df96c01ab7818fd8e14c37d74468a
Mirim An <mimicatcodes@gmail.com>
2016-08-09 08:19:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
-def prime?(number)
-  if number<=1
+def prime?(num)
+  if num <= 1
     return false
   end
 
-(2...number).each do |x|
-   if (number % x == 0) then
-     return false
- end
- end
- true
- end
+  (2...num).each do |x|
+    if num % x == 0 
+    return false
+  end
+end
+true
+end
