ad93878810fa623d171326ec6530d7e307648db0
Zaira Burke <zairaburke@gmail.com>
2016-08-13 01:33:22 +0000

    Prime Solution

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  prime = true
+  x = 2
+  if number <=2
+    prime = false
+  end
+
+  while (x <=number/2)
+    if (number % x == 0)
+      prime = false
+      break
+    end
+    x +=1
+  end
+  return prime
+end
