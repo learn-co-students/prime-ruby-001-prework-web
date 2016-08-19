ffe7467f777af95fee0a4d8be30b909ac580a727
Michael Wiggins <michael.wiggins@me.com>
2016-08-19 15:09:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # you can make your own benchmark
 #http://rubylearning.com/blog/2013/06/19/how-do-i-benchmark-ruby-code/
 #this is a boolean operation - testing, is this shit true?
-#in this first line, it's calling on an operation that has been defined for you and stored already. It's like magic.
+#in this first line, it's calling on a previously defined operation (require "prime") that has been defined for you and stored already. It's like magic.
 require 'prime'
 
 def prime?(num)
