eb5b8bef41d02a10836bd03c02fe26d8e4cd5c1c
Jonathan Peterkin <jonjpeterkin@gmail.com>
2016-09-09 21:48:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+
+
+def prime?(n)
+  checks = [2,3,5,7]
+  return false if n == 1
+  checks.each do |check|
+    return true if n == check
+    return false if n % check == 0
+  end
+  true
+end
