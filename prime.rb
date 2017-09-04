207370ec5385deb725ee17650c058d1b91bf8e64
Jason Kwong <jasonkwong11@gmail.com>
2017-09-03 20:16:28 -0700

    refactor

--- prime.rb
+++ prime.rb
@@ -1,27 +1,3 @@
-def prime?(integer)
-  #take integer argument
-  #divide integer by the range of 2...integer (not including 1 and the integer)
-  #if they ALL return a remainder (modulo=!0), then it is prime. => return true
-  #if at least one returns NO remainder (module = 0), it is not prime => return false
-  if integer <= 1
-    return false
-   elsif integer <= 3
-    return true
-    elsif integer%2 == 0 || integer % 3 == 0
-    return false
-    elsif integer%5==0
-    return false
-  end
-
-  result = []
-  divisor = (5...integer).to_a
-  divisor.each do |x|
-    result.push(integer % x)
-  end
-
-  if result.include? 0
-    return false
-  else
-    return true
-  end
-  end
+def prime?(a)
+  (a == 1 || a == 0) ? false : (2...a).none? {|t| a%t==0}
+end
