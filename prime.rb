6ac80ae7322e92879ae6b658b5ec3f133b65a27c
Steven G. Harms <steven.harms@flatironschool.com>
2018-01-24 11:27:42 -0500

    Implement simplified prime test algorithm

--- prime.rb
+++ prime.rb
@@ -1,34 +1,11 @@
-
-def sieve(max)
-  # Set up an array with all the numbers from 0 to the max
-  primes = (0..max).to_a
-
-  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
-  # aren't prime.
-  primes[0] = primes[1] = nil
-
-  # Iterate through primes array
-  counter = 0
-  primes.each do |p|
-    # Skip if nil
-    next unless p
-
-    # Break if we are past the square root of the max value 
-    break if p*p > max
-    counter += 1
-    # Start at the square of the current number, and step through.
-    # Go up to the max value, by multiples of the current number, and replace
-    # that value with nil in the primes array
-    (p*p).step(max,p) { |m| primes[m] = nil }
-  end
-
-  # Finally, return the compacted array.
-  puts "Solved for #{max} in #{counter} steps."
-  primes.compact
-end
-
 def prime?(num)
-  sieve(num).include?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
 end
 
 puts prime?(105557)
